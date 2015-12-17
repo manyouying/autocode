ddf99cf577db0a5fb2e9d279de59415fad548c45
夏钰辉 <xiayuhui@ippjr.com>
2015-12-17 14:08:14 +0800

    去除注释

--- src/autocode/Generator.java
+++ src/autocode/Generator.java
@@ -1,21 +1,9 @@
-/*
- * Copyright (c) 2015 金联所. All rights reserved.
- *
- * The copyright to the computer software herein is the property of 金联所.
- * The software may be used and/or copied only with the written permission 
- * of 金联所, or in accordance with the terms and conditions stipulated in 
- * the agreement/contract under which the software has been supplied.
- *
- */
 package autocode;
 
 import cn.org.rapid_framework.generator.GeneratorFacade;
 
 /**
- * <一句话功能简述>
- * <功能详细描述>
- * 
- * @author  lenovo 2015年12月17日
+ * @author  xiayuhui 2015年12月17日
  */
 public class Generator {
     
